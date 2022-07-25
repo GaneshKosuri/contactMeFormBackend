0c150112f2e1141be0806596ddb610953e23afc9
Ganesh Kosuri <ganeshkosuri9656@gmail.com>
2022-07-25 16:10:09 +0530

    fix cors issue

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const nodemailer = require("nodemailer");
+const cors = require('cors')
 require('dotenv').config();
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const MAIL = process.env.EMAIL
 const PASSWORD = process.env.PASSWORD
