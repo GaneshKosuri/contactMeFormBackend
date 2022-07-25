99cecec74331e8caecc01f536946b16b42f3d6eb
Ganesh Kosuri <ganeshkosuri9656@gmail.com>
2022-07-25 15:49:03 +0530

    add basic code for form submit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const nodemailer = require("nodemailer");
+require('dotenv').config();
+
+const app = express()
+
+app.use(express.json())
+
+const MAIL = process.env.EMAIL
+const PASSWORD = process.env.PASSWORD
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
+
+app.post("/send-mail",async (request,res)=>{
+    const {name,mail,body} = request.body
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: MAIL,
+            pass: PASSWORD
+        }
+    });
+
+    let info = await transporter.sendMail({
+        from: mail,
+        to: MAIL,
+        subject: `New Query from ${name}`,
+        text: body,
+    }, (err, data) => {
+        if(err) {
+            res.send("Error occured while contacting");
+        } else {
+            res.send('Email sent successfully');
+        }
+    });
+})
